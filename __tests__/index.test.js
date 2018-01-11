7e01891e20801d6d66349f8e3ead275c912cf9d9
Roman Averin <rastler@gmail.com>
2018-01-11 15:28:23 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,43 @@
 import path from 'path';
-import fs from 'fs';
 import genDiff from '../src';
 
-const answerVisual = fs.readFileSync(path.join(__dirname, '__fixtures__/answer.visual.txt'), 'utf-8');
-const answerPlain = fs.readFileSync(path.join(__dirname, '__fixtures__/answer.plain.txt'), 'utf-8');
+const answerVisual =
+  [
+    '{',
+    '   common: {',
+    '      setting1: Value 1',
+    '    - setting2: 200',
+    '      setting3: true',
+    '    - setting6: {',
+    '         key: value',
+    '      }',
+    '    + setting4: blah blah',
+    '    + setting5: {',
+    '         key5: value5',
+    '      }',
+    '   }',
+    '   group1: {',
+    '    - baz: bas',
+    '    + baz: bars',
+    '      foo: bar',
+    '   }',
+    ' - group2: {',
+    '      abc: 12345',
+    '   }',
+    ' + group3: {',
+    '      fee: 100500',
+    '   }',
+    '}',
+  ].join('\n');
+const answerPlain = [
+  'Property \'common.setting2\' was removed\n',
+  'Property \'common.setting6\' was removed\n',
+  'Property \'common.setting4\' was added with value: blah blah\n',
+  'Property \'common.setting5\' was added with complex value\n',
+  'Property \'group1.baz\' was updated. From \'bas\' to \'bars\'\n',
+  'Property \'group2\' was removed\n',
+  'Property \'group3\' was added with complex value\n',
+].join('');
 test('Testing plain output with JSON', () => {
   const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
   const testFile2 = path.join(__dirname, '__fixtures__/after.json');
