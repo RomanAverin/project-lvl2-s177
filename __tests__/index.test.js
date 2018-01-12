46b0b94e26a445f9c9b12d4d572bf711a7ccdd2e
Roman Averin <rastler@gmail.com>
2018-01-12 23:39:09 +0300

    fix side effect

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,39 +2,37 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
 
-const answerFileVisual = path.join(__dirname, '__fixtures__/answer.visual.txt');
-const answerFilePlain = path.join(__dirname, '__fixtures__/answer.plain.txt');
-const answerFileJSON = path.join(__dirname, '__fixtures__/answer.json.txt');
-const answerVisual = fs.readFileSync(answerFileVisual, 'utf-8');
-const answerPlain = fs.readFileSync(answerFilePlain, 'utf-8');
-const answerJSON = fs.readFileSync(answerFileJSON, 'utf-8');
+const fixturesPath = path.resolve(process.cwd(), '__tests__/__fixtures__');
+const answerVisual = fs.readFileSync(`${fixturesPath}/answer.visual.txt`, 'utf-8');
+const answerPlain = fs.readFileSync(`${fixturesPath}/answer.plain.txt`, 'utf-8');
+const answerJSON = fs.readFileSync(`${fixturesPath}/answer.json.txt`, 'utf-8');
 test('Testing plain output with JSON', () => {
-  const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
-  const testFile2 = path.join(__dirname, '__fixtures__/after.json');
-  expect(genDiff(testFile1, testFile2, 'plain')).toEqual(answerPlain);
+  const testFile1Path = `${fixturesPath}/befor.json`;
+  const testFile2Path = `${fixturesPath}/after.json`;
+  expect(genDiff(testFile1Path, testFile2Path, 'plain')).toEqual(answerPlain);
 });
-test('Testing visual output genDiff with JSON', () => {
-  const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
-  const testFile2 = path.join(__dirname, '__fixtures__/after.json');
-  expect(genDiff(testFile1, testFile2, 'visual')).toEqual(answerVisual);
+test('Testing visual output with JSON', () => {
+  const testFile1Path = `${fixturesPath}/befor.json`;
+  const testFile2Path = `${fixturesPath}/after.json`;
+  expect(genDiff(testFile1Path, testFile2Path, 'visual')).toEqual(answerVisual);
 });
 test('Testing visual output genDiff with YML', () => {
-  const testFile1 = path.join(__dirname, '__fixtures__/befor.yml');
-  const testFile2 = path.join(__dirname, '__fixtures__/after.yml');
-  expect(genDiff(testFile1, testFile2)).toEqual(answerVisual);
+  const testFile1Path = `${fixturesPath}/befor.yml`;
+  const testFile2Path = `${fixturesPath}/after.yml`;
+  expect(genDiff(testFile1Path, testFile2Path)).toEqual(answerVisual);
 });
 test('Testing visual output genDiff with JSON and YML', () => {
-  const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
-  const testFile2 = path.join(__dirname, '__fixtures__/after.yml');
-  expect(genDiff(testFile1, testFile2)).toEqual(answerVisual);
+  const testFile1Path = `${fixturesPath}/befor.json`;
+  const testFile2Path = `${fixturesPath}/after.yml`;
+  expect(genDiff(testFile1Path, testFile2Path)).toEqual(answerVisual);
 });
 test('Testing visual output genDiff with INI', () => {
-  const testFile1 = path.join(__dirname, '__fixtures__/befor.ini');
-  const testFile2 = path.join(__dirname, '__fixtures__/after.ini');
-  expect(genDiff(testFile1, testFile2)).toEqual(answerVisual);
+  const testFile1Path = `${fixturesPath}/befor.ini`;
+  const testFile2Path = `${fixturesPath}/after.ini`;
+  expect(genDiff(testFile1Path, testFile2Path)).toEqual(answerVisual);
 });
 test('Testing JSON output genDiff with JSON', () => {
-  const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
-  const testFile2 = path.join(__dirname, '__fixtures__/after.json');
-  expect(genDiff(testFile1, testFile2, 'json')).toEqual(answerJSON);
+  const testFile1Path = `${fixturesPath}/befor.json`;
+  const testFile2Path = `${fixturesPath}/after.json`;
+  expect(genDiff(testFile1Path, testFile2Path, 'json')).toEqual(answerJSON);
 });
