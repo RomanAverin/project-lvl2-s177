94ffc667f491cda851ba7379d4f65f2bf3d07573
Roman Averin <rastler@gmail.com>
2021-08-20 21:33:36 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,10 @@
 #!/usr/bin/env node
 
 import program from 'commander';
-import { readFile } from 'fs/promises';
 
 import gendiff from '../src/index.js';
 
-const packageFile = JSON.parse(
-  await readFile(
-    new URL('../package.json', import.meta.url)
-)
-);
-
 program
-  .version(packageFile.version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format', 'visual')
   .arguments('<firstConfig> <secondConfig>')
